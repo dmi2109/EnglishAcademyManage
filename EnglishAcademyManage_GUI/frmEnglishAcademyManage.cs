09b8a813c75e38281e17a8974099ebc65eb62b4c
Mai Diệu Mi <maidieumi@gmail.com>
2024-10-14 14:41:27 +0700

    first commit

---
+++ EnglishAcademyManage_GUI/frmEnglishAcademyManage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnglishAcademyManage_GUI
+{
+    public partial class frmEnglishAcademyManage : Form
+    {
+        public frmEnglishAcademyManage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
